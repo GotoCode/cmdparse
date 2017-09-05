52f0b4f3fde63434e8a04702051063bd667791f8
GotoCode <khalid.peer233@gmail.com>
2017-09-05 10:12:24 +0300

    add main file for parser

---
+++ main.py
@@ -0,0 +1,9 @@
+#
+# cmdparse
+#
+# this utlity extracts the command name, positional
+# arguments, and optional arguments (short & long form)
+# from a given string representing a command-line input 
+#
+
+
